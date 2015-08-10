3f254c3638b3fb895459ae720ab2e37654908836
mattscilipoti <matt.scilipoti@ga.co>
2015-08-09 20:53:42 -0400

    artists: support json

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,46 +1,74 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
+  # GET /artists
+  # GET /artists.json
   def index
     @artists = Artist.all
   end
 
+  # GET /artists/1
+  # GET /artists/1.json
+  def show
+  end
+
+  # GET /artists/new
   def new
     @artist = Artist.new
   end
 
+  # GET /artists/1/edit
+  def edit
+  end
+
+  # POST /artists
+  # POST /artists.json
   def create
     @artist = Artist.new(artist_params)
-    if @artist.save
-      redirect_to (artist_path(@artist))
-    else
-      render :new
+
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to @artist, notice: 'Artist was successfully created.' }
+        format.json { render :show, status: :created, location: @artist }
+      else
+        format.html { render :new }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  def show
-    @artist = Artist.find(params[:id])
-  end
-
-  def edit
-    @artist = Artist.find(params[:id])
-  end
-
+  # PATCH/PUT /artists/1
+  # PATCH/PUT /artists/1.json
   def update
-    @artist = Artist.find(params[:id])
-    if @artist.update(artist_params)
-      redirect_to artist_path(@artist)
-    else
-      render :edit
+    respond_to do |format|
+      if @artist.update(artist_params)
+        format.html { redirect_to @artist, notice: 'Artist was successfully updated.' }
+        format.json { render :show, status: :ok, location: @artist }
+      else
+        format.html { render :edit }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
     end
   end
 
+  # DELETE /artists/1
+  # DELETE /artists/1.json
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
-    redirect_to artists_path
+    respond_to do |format|
+      format.html { redirect_to artists_url, notice: 'Artist was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
-  def artist_params
-    params.require(:artist).permit(:name, :photo_url, :nationality)
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def artist_params
+      params[:artist]
+    end
 end
