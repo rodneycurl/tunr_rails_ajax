14723e646812a2c6258197933ebe5630c6eae522
Andy <andrew.sunglae.kim@gmail.com>
2015-08-10 20:19:04 -0400

    add strong params

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -77,6 +77,6 @@ class ArtistsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def artist_params
-      params[:artist]
+      params.require(:artist).permit(:name, :photo_url, :nationality)
     end
 end
