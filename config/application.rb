ea517b4eec44bd0843156ac1a43009ea082b48af
Adrian Maseda <amaseda@gmail.com>
2016-06-14 13:55:57 -0400

    add put patch delete to cors

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Tunr
   class Application < Rails::Application
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      'Access-Control-Request-Method' => %w{GET POST PUT PATCH DELETE OPTIONS}.join(",")
     }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
