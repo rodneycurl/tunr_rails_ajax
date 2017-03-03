eb02af5e0d50e1d22327287ec90774810dfe9b7c
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-03-03 11:38:50 -0500

    updated the test_ajax views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -69,6 +69,9 @@ class ArtistsController < ApplicationController
     end
   end
 
+  def test_ajax
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_artist
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -31,9 +30,8 @@ ActiveRecord::Schema.define(version: 20150808000849) do
     t.integer  "artist_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
-
-  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
 
   add_foreign_key "songs", "artists"
 end
